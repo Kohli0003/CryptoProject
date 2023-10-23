d5a4296db951f9c8675a77c4e45593c8e1087614
Kohli0003 <aakarshitkohli26@gmail.com>
2023-10-23 19:46:12 +0530

    Added Navbar and some css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import { Button, Menu,Typography,Avatar } from 'antd';
-import { Link} from 'react-router-dom';
-import {HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined} from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import {HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined, LoginOutlined} from '@ant-design/icons';
+import icon from '../images/cryptologo.png';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='nav-container'>
+        <div className="logo-container">
+            <Avatar src={icon} size="large"/>
+            <Typography.Title level={2} className="logo">
+                <Link to="/" CryptoWorld>
+                </Link>
+            </Typography.Title>
+            
+        </div>
+        <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined/>}>
+                <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>}>
+                <Link to="/">CryptoCrurecncy</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                <Link to="/">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>}>
+                <Link to="/">News</Link>
+                </Menu.Item>
+           
+
+        </Menu>
       
     </div>
   )
